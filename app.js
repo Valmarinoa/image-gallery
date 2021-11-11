c633421bbd09f97586757c218ad83411d54ae444
Valmarinoa <valmarinocol@gmail.com>
2021-11-11 10:17:03 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const mainGallery = document.getElementById("mainGallery");
+const modal = document.getElementById("modal");
+const selectedVisual = document.getElementById("selectedVisual");
+
+//Iterate - map through Images and assign each a source.
+const index = [1, 2, 3, 4, 5, 6, 7, 8];
