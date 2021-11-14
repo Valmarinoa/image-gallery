b3441f0c3fbf53cf78725ee2cb22b283f31e42e8
Valmarinoa <valmarinocol@gmail.com>
2021-11-14 22:03:49 +0100

    Set up structure and styling for image gallery. for
    Created  gallery which will eventually hold each individual image, along with its caption and respective download button.

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-const mainGallery = document.getElementById("mainGallery");
+//refferencing DOM elements.
+const mainGallery = document.getElementById("container");
+const contentModal = document.getElementById("content-modal");
 const modal = document.getElementById("modal");
-const selectedVisual = document.getElementById("selectedVisual");
+const selectedVisual = document.getElementById("selectedCoversVisual");
+const link = document.getElementById("a");
+const cover = document.getElementsByClassName("selectedCoverImage");
 
-//Iterate - map through Images and assign each a source.
-const index = [1, 2, 3, 4, 5, 6, 7, 8];
+const index = [45, 60, 52, 53, 55, 56, 61, 62, 58];
+
+//create a fucntion to iterate through index, and that can reffer to each element a specific image source.
+//index.forEach((issue) => {
+function triggerModal(event) {
+  event.preventDefault();
+  index.forEach((issue) => {
+    modal.style.visibility = "visible";
+    selectedVisual.src = `media/issue_${issue}.png`;
+    selectedVisual.alt = `Selected cover`;
+  });
+}
+/*cover.addEventListener("click", () => {
+  //modal pops up when clicking on image
+  index.forEach((issue) => {
+    modal.style.visibility = "visible";
+    selectedVisual.src = `media/issue_${issue}.png`;
+    selectedVisual.alt = `Selected cover`;
+  });
+//});*/
+
+modal.addEventListener("click", () => {
+  modal.style.visibility = "hidden";
+  modal.src = "";
+  modal.alt = "";
+});
